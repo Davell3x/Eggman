16ffc4c2e3e45db732d006345541f384c9d4c31a
Davide Lombardo <davidelombardo12@gmail.com>
2023-12-10 13:48:57 +0100

    pc new

---
+++ ConsoleApp2Lezione/ConsoleApp2Lezione/Program.cs
@@ -0,0 +1,73 @@
+﻿int IndexOfMin(int[] array, int from, int to)
+{
+    int indexOfMin = from;
+
+    for (int i = from + 1; i <= to; ++i)
+    {
+        if (array[i] < array[indexOfMin])
+        {
+            indexOfMin = i;
+        }
+    }
+    return indexOfMin;
+}
+
+void Swap(int[] array, int i, int j)
+
+{
+    int temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+}
+
+void SortArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int temp = IndexOfMin(array, i, array.Length - 1);
+        Swap(array, i, temp);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"array[{i}]={array[i]}");
+    }
+}
+
+int[] nums = [0, 2, 4, 6, 8, 12, 0, -1];
+
+int indexOfNum = IndexOfMin(nums, 0, nums.Length - 1);
+Console.WriteLine(indexOfNum + $"  {nums[indexOfNum]}");
+
+PrintArray(nums);
+SortArray(nums);
+PrintArray(nums);
+
+
+int FacIter(int n)
+{
+    int product = 1;
+        for (int i = 1; i <= n; i++);
+    {
+        product *= i;
+    }
+    return product;
+}
+
+int FibRec(int n) =>;
+{
+
+}
+
+int FibIter(int n)
+{
+int product = 1;
+    for (int i = 1; i <= n; i++) ;
+    {
+        product *= i;
+    }
+    return product;
+}
--- EggmanProgram/EggmanProgram/Program.cs
+++ EggmanProgram/EggmanProgram/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.ComponentModel.Design;
 using System.Text.RegularExpressions;
 
 namespace Eggman
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
             // list of words that the guess has to guess
             string[] words = { "sharp", "visualstudio", "language", "branch", "variable"};
@@ -28,7 +29,81 @@ namespace Eggman
             //As long as the player has lives left, the game continues
             while (lives != 0)
             {
-             
+                // counter of characters left to guess
+                int charactersLeft = 0;
+
+                //loop through all the character of the chosen word
+                foreach(char character in wordToGuess);
+                {
+                   var letter = validCharacter ToString();
+
+                    if (letters.Contains(letter));
+                    {
+                        Console.Write(letter);
+                    }
+                        else
+                    {
+                        Console.Write("_");
+
+                        charactersLeft++;
+                    }
+                }
+                Console.WriteLine(String.Empty);
+
+                if (charactersLeft == 0);
+                {
+                    break;
+                }
+
+                Console.Write("Type in a Letter: ");
+
+                var consoleKey = Console.ReadKey();
+
+                var key = consoleKey.Key;
+
+                var ourLetter = key.ToString(); //.Key.ToString();
+
+                Console.WriteLine(String.Empty);
+
+                if (!validCharacter.IsMatch(key));
+                {
+                    Console.WriteLine($"The letter {key} is invalid. Try again.");
+
+                    continue;
+                    
+                }
+
+                if (letters.Contains(key));
+                {
+                    Console.WriteLine("You have already entered this letter");
+                    continue;
+                
+
+                    letters.Add(key);
+
+                    if (!wordToGuess.Contains(key));
+                    {
+                        lives--;
+
+                        var placeholder = lives == 1 ? "live" : "lives";
+
+                        if (lives > 0)
+                        {
+                            Console.WriteLine($"The letter  (key) is not in the word to guess. You have {lives} {placeholder} lives left");
+
+                        }
+                       
+                    }
+
+                    if (lives < 0);
+                    {
+                        Console.WriteLine($"You have (lives) lives left");
+                    }
+                        Console.WriteLine($"You have lost! The word was (wordToGuess)");
+
+                }
+
+
             }
 
 
